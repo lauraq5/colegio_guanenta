02f94226d41551bf06843745515f108a346b74d4
lauraq5 <lauraquintana@colegioguanenta.edu.co>
2023-05-18 16:42:04 -0500

    se modifico el archivo

--- colegio _guanenta.py
+++ colegio _guanenta.py
@@ -1,138 +1,95 @@
-#---------------------------------
-# estudiante guanentino
-#---------------------------------
-
-# se importa la libreria tkinter con todas sus funciones
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
 
-#-------------------------
-# funciones de la app
-#-------------------------
-
-# abrir toplevel informacion estudiante
-def abrir_toplevel_informacion_estudiante():
-    global toplevel_resultados
-    toplevel_resultados = Toplevel()
-    toplevel_resultados.title("informacion")
-    toplevel_resultados.resizable(False, False)
-    toplevel_resultados.geometry("500*500")
-    toplevel_resultados.config(bg="deep pink")
-
-    # logo de la app
-    logo = PhotoImage(file="estudiantes.png")
-    lb_logo2 = Label(toplevel_resultados, image=logo, bg="white")
-    lb_logo2.place(x=250,y=200)
-
-    # etiqueta la informacion del estudiante del colegio guanenta
-    lb_c= Label(toplevel_resultados, text = "nombre" = ")
-    lb_c.informacion(bg="salmon", fg="cyan2", font=("Helvetica", 40))
-    lb_c. accion(x=150, y=100)
-
-    # caja para poder hallar el valor del estudiante del colegio guanenta
-    entry_c = Entry(toplevel_resultados, textvariable=a)
-    entry_c.config(bg="white", fg="blue", font=("Times New Roma", 18), width=6)
-    entry_c.focus_set()
-    entry_c.place(x=210,y=60)
-
-   # boton para convertir
-    bt_aceptar = Button(toplevel_resultados,text="Aceptar", command=aceptar)
-    bt_aceptar.place(x=100, y=150, width=100, height=30)
-
-# aceptar
-def aceptar():
-    global cent
-    cent = int(c.get())
-    toplevel_resultados.destroy()
-
-# convertir
-def convertir():
-    messagebox.showinfo("datos del estudiante del colegio san jose de guanenta")
-    # cent = int(c.get())
-    if kf.get()=="nombre":
-        n = nombre + apellido 
-        t_resultados.insert(INSERT, f"\n{int(c.get())} a equivalen a {n} n")
-    elif kf.get() == "apellido":
-        f = apellido
-        t_resultados.insert(INSERT, f"\n{int(c.get())} a equivalen a {a} a")
-    else:
-        t_resultados.insert(INSERT, "Debe seleccionar una opción")
+def calcular_notas():
+    nota_1 = float(entry_nota_1.get())
+    nota_2 = float(entry_nota_2.get())
+    nota_3 = float(entry_nota_3.get())
     
-# borrar
-def borrar():
-    messagebox.showinfo("estudiante_guanentino", "Los datos serán borrados")
-    c.set("")
-    t_resultados.delete("1.0","end")
+    promedio = (nota_1 + nota_2 + nota_3 ) / 3
+    
+    if promedio >= 4.5:
+        mensaje_nota = "excelente"
+    elif promedio >= 3.0:
+        mensaje_nota = "Aprobado superior"
+    else:
+        mensaje_nota = "Reprobado bajo"
+    
+    messagebox.showinfo("Resultado", f"Promedio de notas: {promedio}\n{mensaje_nota}")
+    
 
-# salir
-def salir():
-    messagebox.showinfo("estudiante_guanentino", "La app se va a cerrar")
-    ventana_principal.destroy()
+def calcular_masa_corporal():
+    peso = float(entry_peso.get())
+    altura = float(entry_altura.get())
+    
+    masa_corporal = peso / (altura ** 2)
+    
+    if masa_corporal < 18.5:
+        estado = "Bajo peso"
+    elif masa_corporal < 25:
+        estado = "Peso normal"
+    elif masa_corporal < 30:
+        estado = "Sobrepeso"
+    else:
+        estado = "Obesidad"
+    
+    messagebox.showinfo("Resultado", f"Índice de masa corporal: {masa_corporal:.2f}\nEstado: {estado}")
+    
 
-#-----------------------------
-# ventana principal de la app
-#-----------------------------
+def calcular_salud():
+    # Aquí puedes incluir tu propia lógica para evaluar la salud del estudiante
+    # y mostrar el resultado utilizando messagebox.showinfo()
+    pass
 
-# se declara una variable llamada ventana_principal, que adquiere las caracteristicas de un objeto Tk()
-ventana_principal = Tk()
 
-# titulo de la ventana
-ventana_principal.title("datos del estudiante guanenta")
-# tamaño de la ventana
-ventana_principal.geometry("500x500")
+# Crear la ventana principal
+root = tk.Tk()
+root.title("Cálculo de notas, masa corporal y salud")
 
-# deshabilitar boton de maximizar
-ventana_principal.resizable(False, False)
+# Etiquetas y campos de entrada para las notas
+label_notas = tk.Label(root, text="Notas del estudiante")
+label_notas.grid(row=0, column=0, padx=10, pady=5)
 
-# color de fondo de la ventana
-ventana_principal.config(bg="deep pink")
+label_nota_1 = tk.Label(root, text="Nota 1:")
+label_nota_1.grid(row=1, column=0, padx=10, pady=5)
+entry_nota_1 = tk.Entry(root)
+entry_nota_1.grid(row=1, column=1, padx=10, pady=5)
 
-#--------------------------------
-# variables del estudiando 
-#--------------------------------
-n = StringVar()
-a = StringVar()
-global logo
+label_nota_2 = tk.Label(root, text="Nota 2:")
+label_nota_2.grid(row=2, column=0, padx=10, pady=5)
+entry_nota_2 = tk.Entry(root)
+entry_nota_2.grid(row=2, column=1, padx=10, pady=5)
 
-#--------------------------------
-# frame entrada datos
-#--------------------------------
-frame_entrada = datos(ventana_principal)
-frame_entrada.datos(bg="yellow", width=480, height=180)
-frame_entrada.datos(x=10, y=10)
+label_nota_3 = tk.Label(root, text="Nota 3:")
+label_nota_3.grid(row=3, column=0, padx=10, pady=5)
+entry_nota_3 = tk.Entry(root)
+entry_nota_3.grid(row=3, column=1, padx=10, pady=5)
 
-# titulo de la app
-titulo =nombre(frame_entrada, text="Termperatura 1.0")
-titulo.nombre(bg = "white",fg="blue", font=("Helvetica", 20))
-titulo.nombre(x=240,y=10)
+btn_calcular_notas = tk.Button(root, text="Calcular Notas", command=calcular_notas)
+btn_calcular_notas.grid(row=4, column=0, columnspan=2, padx=10, pady=5)
 
-# boton para abrir Toplevel para ingresar °C
-bt_centigrados = Button(frame_entrada, text="Ingresar °C", command=abrir_toplevel_informacion_estudiante)
-bt_centigrados.place(x=240, y=60)
 
-# radiobutton para kelvin
-rb_k = Radiobutton(frame_entrada, text="Kelvin", variable=kf, value="kelvin")
-rb_k.config(bg="white", fg="blue", font=("Helvetica", 18))
-rb_k.place(x=240, y=110)
+# Etiquetas y campos de entrada para la masa corporal
+label_masa_corporal = tk.Label(root, text="Masa corporal del estudiante")
+label_masa_corporal.grid(row=5, column=0, padx=10, pady=5)
 
-# radiobutton para farenheit
-rb_f = Radiobutton(frame_entrada, text="Fahrenheit", variable=kf, value="fahrenheit")
-rb_f.config(bg="white", fg="blue", font=("Helvetica", 18))
-rb_f.place(x=240, y=140)
+label_peso = tk.Label(root, text="Peso (kg):")
+label_peso.grid(row=6, column=0, padx=10, pady=5)
+entry_peso = tk.Entry(root)
+entry_peso.grid(row=6, column=1, padx=10, pady=5)
 
-#--------------------------------
-# frame operaciones
-#--------------------------------
-frame_operaciones = Frame(ventana_principal)
-frame_operaciones.config(bg="white", width=480, height=100)
-frame_operaciones.place(x=10, y=200)
+label_altura = tk.Label(root, text="Altura (m):")
+label_altura.grid(row=7, column=0, padx=10, pady=5)
+entry_altura = tk.Entry(root)
+entry_altura.grid(row=7, column=1, padx=10, pady=5)
 
-# boton para convertir
-bt_convertir = Button(frame_operaciones,text="Convertir", command=convertir)
-bt_convertir.place(x=45, y=35, width=100, height=30)
+btn_calcular_masa_corporal = tk.Button(root, text="Calcular Masa Corporal", command=calcular_masa_corporal)
+btn_calcular_masa_corporal.grid(row=8, column=0, columnspan=2, padx=10, pady=5)
 
-# boton para borrar
-bt_borrar = Button(frame_operaciones, text="Borrar", command=borrar)
-bt_borrar.place(x=190, y=35, width=100,height=100)
 
-ventana_principal.mainloop()
+# Botón para calcular la salud
+btn_calcular_salud = tk.Button(root, text="Calcular Salud", command=calcular_salud)
+btn_calcular_salud.grid(row=9, column=0, columnspan=2, padx=10, pady=5)
+
+root.mainloop()
+
